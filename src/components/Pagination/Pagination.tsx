b8f0d5d8a6af2f9e107cb4af4f285e9019545eb5
Web Dev <tom2kota@gmail.com>
2020-08-13 17:17:13 +0300

    Update files

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -2,9 +2,7 @@ import React, {useEffect, useRef, useState} from "react";
 import ResizeObserver from 'resize-observer-polyfill';
 import './Pagination.css'
 
-
 const Pagination = () => {
-
     const items = [
         'all',
         'new games',
@@ -15,53 +13,38 @@ const Pagination = () => {
         'top 2020'
     ]
 
-
     const itemRef = useRef(null);
-
     const [value, setValue] = useState(0);
 
     useEffect(() => {
-
-
         const resizeObserver = new ResizeObserver((entries) => {
             console.log('resizeObserver => .Pagination-list width: ', entries.map(x => x.target.clientWidth));
         });
-
         // @ts-ignore
         resizeObserver.observe(itemRef.current);
-
         if (value === 0) {
-
             // @ts-ignore
             let computedWidth = Number.parseInt(window.getComputedStyle(itemRef.current, null).width)
             setValue(computedWidth);
-
         }
     }, [value]);
 
+    const renderItems = () => items.map(item => {
+        return (
+            <div key={Math.floor(Math.random() * 90000)}
+                 className="Pagination-items">{item}
+            </div>)
+    })
 
     return (
-
         <>
             <div className="Pagination">
-
                 <div className="Pagination-controls left">&lsaquo;</div>
-
-
                 <div ref={itemRef} className="Pagination-list">
-                    {items.map(item => <div key={Math.floor(Math.random() * 10000)}
-                                            className="Pagination-items">{item}</div>)}
+                    {renderItems()}
                 </div>
-
                 <div className="Pagination-controls right">&rsaquo;</div>
             </div>
-            <div>
-                <button onClick={() => setValue(0)}>
-                    <div>value: {value}</div>
-                </button>
-            </div>
-
-
         </>
     )
 }
