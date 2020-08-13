444288fbac88497905fecb6a3aec9fcce3da92bd
Web Dev <tom2kota@gmail.com>
2020-08-13 18:01:00 +0300

    Update files

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -13,26 +13,46 @@ const Pagination = () => {
         'top 2020'
     ]
 
+    const listRef = useRef(null);
     const itemRef = useRef(null);
     const [value, setValue] = useState(0);
+    const [val, setVal] = useState(0);
+    const [paginationListValue, setPaginationListValue] = useState(0);
 
     useEffect(() => {
-        const resizeObserver = new ResizeObserver((entries) => {
-            console.log('resizeObserver => .Pagination-list width: ', entries.map(x => x.target.clientWidth));
+
+        const resizeListObserver = new ResizeObserver((entries) => {
+            // console.log('resizeObserver => .Pagination-list width: ', entries.map(x => x.target.clientWidth));
+            // @ts-ignore
+            setPaginationListValue(entries.map(x => x.target.clientWidth))
         });
         // @ts-ignore
-        resizeObserver.observe(itemRef.current);
+        resizeListObserver.observe(listRef.current);
+
+
         if (value === 0) {
             // @ts-ignore
-            let computedWidth = Number.parseInt(window.getComputedStyle(itemRef.current, null).width)
+            let computedWidth = Number.parseInt(window.getComputedStyle(listRef.current, null).width);
             setValue(computedWidth);
         }
-    }, [value]);
+
+
+        if (val === 0) {
+            // @ts-ignore
+            let computedWidth = Number.parseInt(window.getComputedStyle(itemRef.current, null).width);
+            setVal(computedWidth);
+        }
+
+
+    }, [value, val]);
+
 
     const renderItems = () => items.map(item => {
+
+
         return (
-            <div key={Math.floor(Math.random() * 90000)}
-                 className="Pagination-items">{item}
+            <div ref={itemRef} key={Math.floor(Math.random() * 90000)} className="Pagination-items">
+                {item}
             </div>)
     })
 
@@ -40,11 +60,14 @@ const Pagination = () => {
         <>
             <div className="Pagination">
                 <div className="Pagination-controls left">&lsaquo;</div>
-                <div ref={itemRef} className="Pagination-list">
+                <div ref={listRef} className="Pagination-list">
                     {renderItems()}
                 </div>
                 <div className="Pagination-controls right">&rsaquo;</div>
             </div>
+
+            <div>{paginationListValue}</div>
+            <div>{val}</div>
         </>
     )
 }
